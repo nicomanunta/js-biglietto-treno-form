98351a8d0fbbdbeee7b020bd07cc5de2706ffaa5
nicomanunta <nic.manun@gmail.com>
2023-11-21 15:37:25 +0100

    Milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -1,39 +1,77 @@
 // prompt per richiedere l'età
-let eta = prompt("Inserire l'età")
-console.log(eta)
-// prompt per richiedere numero di km da percorrere
-let numero_km = prompt("Inserire il numero di km da percorrere?")
-console.log(numero_km)
+// let eta = document.getElementById("eta").innerHTML
+// console.log(eta)
+// // prompt per richiedere numero di km da percorrere
+// let chilometri =
+// console.log(numero_km)
 // calcolare il prezzo del viaggio moltiplicando il numero dei km per il prezzo al km 0.21 € 
-let biglietto = numero_km * 0.21
 
 
-let sconto;
-let message;
-if (eta < 18){
-    message = "Hai diritto ad uno sconto del 20%. Prezzo: "
-    let biglietto_scontato = biglietto - biglietto * 20 / 100
-    console.log(biglietto_scontato)
-    // limitare le cifre decimali fino a centesimi
-    let biglietto_finale = biglietto_scontato.toFixed(2)
-    console.log(biglietto_finale)
- 
+
+
+// let sconto;
+// let message;
+// if (eta < 18){
+//     message = "Hai diritto ad uno sconto del 20%. Prezzo: "
+//     let biglietto_scontato = biglietto - biglietto * 20 / 100
+//     console.log(biglietto_scontato)
+//     // limitare le cifre decimali fino a centesimi
+//     let biglietto_finale = biglietto_scontato.toFixed(2)
+//     console.log(biglietto_finale)
     
-}
-else if (eta > 65){
-    message = "Hai diritto ad uno sconto del 40%. Prezzo: "
-    let biglietto_scontato = biglietto - biglietto * 40 / 100
-    console.log(biglietto_scontato)
-    // limitare le cifre decimali fino a centesimi
-    let biglietto_finale = biglietto_scontato.toFixed(2)
-    console.log(biglietto_finale)
-}
-else{
-    biglietto = numero_km * 0.21;
-    // limitare le cifre decimali fino a centesimi
-    let biglietto_finale = biglietto.toFixed(2)
-    console.log(biglietto_finale)
     
-}
+// }
+// else if (eta > 65){
+//     message = "Hai diritto ad uno sconto del 40%. Prezzo: "
+//     let biglietto_scontato = biglietto - biglietto * 40 / 100
+//     console.log(biglietto_scontato)
+//     // limitare le cifre decimali fino a centesimi
+//     let biglietto_finale = biglietto_scontato.toFixed(2)
+//     console.log(biglietto_finale)
+// }
+// else{
+//     biglietto = chilometri * 0.21;
+//     // limitare le cifre decimali fino a centesimi
+//     let biglietto_finale = biglietto.toFixed(2)
+//     console.log(biglietto_finale)
+    
+// }
+
+
+
+let button = document.querySelector(".btn")
+button.addEventListener("click", function(){
+
+    let chilometri = document.getElementById("chilometri").value
+    let eta = document.getElementById("eta").value;
+    let biglietto = chilometri * 0.21
+
+    
+    if (eta < 18){
+        message = "Hai diritto ad uno sconto del 20%. Prezzo: "
+        let biglietto_scontato = biglietto - biglietto * 20 / 100
+        console.log(biglietto_scontato)
+        // limitare le cifre decimali fino a centesimi
+        let biglietto_finale = biglietto_scontato.toFixed(2)
+        console.log(biglietto_finale)
+     
+        
+    }
+    else if (eta > 65){
+        message = "Hai diritto ad uno sconto del 40%. Prezzo: "
+        let biglietto_scontato = biglietto - biglietto * 40 / 100
+        console.log(biglietto_scontato)
+        // limitare le cifre decimali fino a centesimi
+        let biglietto_finale = biglietto_scontato.toFixed(2)
+        console.log(biglietto_finale)
+    }
+    else{
+        biglietto = chilometri * 0.21;
+        // limitare le cifre decimali fino a centesimi
+        let biglietto_finale = biglietto.toFixed(2)
+        console.log(biglietto_finale)
+        
+    }
+})
 
 
